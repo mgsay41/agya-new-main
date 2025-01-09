e5e9efcdc579a5535cae4bebd9c9f139035bf6e8
mahmoudabs2002 <m.mohamed.absai@outlook.com>
2025-01-09 13:37:33 +0200

    Update Report.js

--- backend/models/Report.js
+++ backend/models/Report.js
@@ -4,6 +4,7 @@ const ReportSchema = new mongoose.Schema({
   username: { type: String, required: true }, // Store the user's name
   userImage: { type: String, required: true },
   content: { type: String, required: true },
+  isRead : {type: String, default:false },
   articleId: { type: mongoose.Schema.Types.ObjectId, ref: "Article" },
   postId: { type: mongoose.Schema.Types.ObjectId, ref: "Post" },
   commentId: { type: mongoose.Schema.Types.ObjectId, ref: "Comment" },
