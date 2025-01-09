d9e0495704c6a8784019941a9ffa219776ec5a3b
mahmoudabs2002 <m.mohamed.absai@outlook.com>
2025-01-09 16:31:42 +0200

    Update auth.js

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,11 +1,13 @@
 import express  from "express";
-import { deleteUser, login, Logout, register , ResetPassword, SingleUser,updatedUser } from "../controllers/auth.js";
+import { deleteUser, login, Logout, register , ResetPassword , Adminlogin, ResetPasswordAdmin, SingleUser,updatedUser } from "../controllers/auth.js";
 const router = express.Router();
 import isAuth from '../middleware/auth.js';
 
 router.post("/register", register) // register user
 
 router.post("/login",login) // login user
+router.post("/admin-login",Adminlogin) 
+
 
 router.post("/logout", Logout)// logout user
 router.post("/reset-password", ResetPassword)// logout user
@@ -15,6 +17,7 @@ router.get('/single-user/:id', SingleUser)
 //============ DELETE =========
 
 router.delete("/delete-user/:id", deleteUser) // use to delete user 
+router.post("/reset-password/:id",  ResetPasswordAdmin);
 
 
 
