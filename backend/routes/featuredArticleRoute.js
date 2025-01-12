bf3255a4a016e7238002894ada3bc0aef881fcf3
mahmoudabs2002 <m.mohamed.absai@outlook.com>
2025-01-12 16:25:50 +0200

    Update featuredArticleRoute.js

--- backend/routes/featuredArticleRoute.js
+++ backend/routes/featuredArticleRoute.js
@@ -1,10 +1,11 @@
 import express  from "express";
-import { add,deleteFeatured,getAll} from "../controllers/Featured.js";
+import { add,deleteFeatured,getAll,update} from "../controllers/Featured.js";
 const router = express.Router();
 
 // User management routes
 router.post('/addFeatured', add);
 router.delete("/deleteFeatured", deleteFeatured);
 router.get('/all-featured', getAll);
+router.put('/update-featured', update);
 
 export default router;
