05df6d2e8652a2d7a683bdcd79c6ea9935cd8514
mahmoudabs2002 <m.mohamed.absai@outlook.com>
2025-01-12 11:54:30 +0200

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -39,12 +39,11 @@ app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({ extended: false }));
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');  // Allow all origins
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
+app.use(
+  cors({
+    credentials: true,
+    origin:"*" })
+);
 
 app.use(morgan("dev")); // Log HTTP requests
 
